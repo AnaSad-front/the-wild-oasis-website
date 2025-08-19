7dee6e6e711d5cc876f17af9d28909f37026cf09
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-08-19 18:05:04 +0300

    Updating a reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuestAction(formData) {
   const session = await auth();
@@ -30,6 +31,46 @@ export async function updateGuestAction(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function updateReservation(formData) {
+  const bookingId = Number(formData.get("bookingId"));
+
+  // 1) Authentication
+  const session = await auth();
+  if (!session)
+    throw new Error("You must be logged in to update your profile.");
+
+  // 2) Authorization
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to update this reservation");
+
+  // 3) Building update data
+  const updateData = {
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+  };
+
+  // 4) Mutation
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId)
+    .select()
+    .single();
+
+  // 5) Error handling
+  if (error) throw new Error("Booking could not be updated");
+
+  // 6) Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  // 7) Redirecting
+  redirect("/account/reservations");
+}
+
 export async function deleteReservation(bookingId) {
   const session = await auth();
   if (!session)
@@ -39,7 +80,7 @@ export async function deleteReservation(bookingId) {
   const guestBookingIds = guestBookings.map((booking) => booking.id);
 
   if (!guestBookingIds.includes(bookingId))
-    throw new Error("You are not allowed to delete this booking");
+    throw new Error("You are not allowed to delete this reservation");
 
   const { error } = await supabase
     .from("bookings")
