6e556b9ce3d74ef5cca654fd01dd48b04e144612
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-08-20 14:09:42 +0300

    Creating a new reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -31,6 +31,34 @@ export async function updateGuestAction(formData) {
   revalidatePath("/account/profile");
 }
 
+export async function createReservation(bookingData, formData) {
+  const session = await auth();
+  if (!session)
+    throw new Error("You must be logged in to update your profile.");
+
+  // Object.entries(formData.entries())
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+
+  redirect("/cabins/thankyou");
+}
+
 export async function updateReservation(formData) {
   const bookingId = Number(formData.get("bookingId"));
 
